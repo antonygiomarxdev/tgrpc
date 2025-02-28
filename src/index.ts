16330eeef8f0ec7d52d61fc7c0920153faa671d3
Antony Giomar Hernández <160435936+antonygiomarxdev@users.noreply.github.com>
2025-02-28 16:03:17 -0600

    feat: add createTGRpcServer factory to instantiate Tygrpc server components

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+import { TygrpcServer } from "./tygrpc-server";
+import {ReflectServiceRegistry} from "./features/decorators/infrastructure/reflect-service-registry";
+import {BasicProtoGenerator} from "./features/proto-generation/infrastructure/basic-proto-generator";
+import {GrpcServer} from "./features/server/infrastructure/grpc-server";
+
+export const createTGRpcServer = () => {
+    const serviceRegistry = new ReflectServiceRegistry();
+    const protoGenerator = new BasicProtoGenerator();
+    const server = new GrpcServer(serviceRegistry.getAll(), []);
+
+    return new TygrpcServer(serviceRegistry, protoGenerator, server);
+};
